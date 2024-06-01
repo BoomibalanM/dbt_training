752e3cdcc13adf201dab792fa8630d81db3f219e
Boomi infotech <Boomiinfotech368@gmail.com>
2024-06-01 06:19:32 +0000

    commit

--- models/raw/orders.sql
+++ models/raw/orders.sql
@@ -1,24 +1,8 @@
-select
 
-o.order_id,
-o.order_date,
-o.ship_date,
-o.ship_mode,
-o.ORDER_SELLING_PRICE,
-o.ORDER_COST_PRICE,
-c.customerid,
-c.customername,
-c.segment,
-c.country,
-p.productid,
-p.category,
-p.productname,
-p.subcategory,
-(o.ORDER_SELLING_PRICE - o.ORDER_COST_PRICE) as order_profit
-from {{ ref('orders') }} as o
-left join {{ ref('customer_dbt') }} as c
-on
-o.customerid = c.customer_id
-left join {{ ref('product') }} as p
-on 
-p.productid = o.product_id
+{{
+    config(
+        materialized = 'table'
+    )
+}}
+
+select * from BOOMI.BOOMI_BALAN.orders
